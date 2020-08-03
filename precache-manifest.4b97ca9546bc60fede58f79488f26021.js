b324bdee05b8b08de7c1c0ae6bc3693dd16da55a
Manish166 <capt.mksingh166@gmail.com>
2020-08-03 14:31:58 +0800

    Updates

---
+++ precache-manifest.4b97ca9546bc60fede58f79488f26021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc747a94451a0ad6a63c24576c949b2",
+    "url": "/loginPageTest/index.html"
+  },
+  {
+    "revision": "e89db82cb99a54b8bc9a",
+    "url": "/loginPageTest/static/css/main.630d7b9b.chunk.css"
+  },
+  {
+    "revision": "403c6aa052959f99d593",
+    "url": "/loginPageTest/static/js/2.70e6700e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/loginPageTest/static/js/2.70e6700e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89db82cb99a54b8bc9a",
+    "url": "/loginPageTest/static/js/main.72a4096d.chunk.js"
+  },
+  {
+    "revision": "a69205608125225620eb",
+    "url": "/loginPageTest/static/js/runtime-main.25fcb10f.js"
+  }
+]);
